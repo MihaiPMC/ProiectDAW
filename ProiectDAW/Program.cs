8f19b7a7ff6e070e0814d4c475739d1ff34c46b4
Mihai Patru <mihaipatru05@gmail.com>
2026-01-11 05:01:05 +0200

    feat: Add Docker support with Dockerfile and docker-compose configuration

--- ProiectDAW/Program.cs
+++ ProiectDAW/Program.cs
@@ -60,6 +60,8 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
     try
     {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        await context.Database.MigrateAsync();
         await SeedData.InitializeAsync(services);
     }
     catch (Exception ex)
--- add_updated_at.sql
+++
@@ -1,4 +0,0 @@
--- Adaugă UpdatedAt la GroupMessages dacă nu există
--- Notă: SQLite va da eroare dacă coloana există deja, dar vom ignora
-ALTER TABLE GroupMessages ADD COLUMN UpdatedAt TEXT;
-
